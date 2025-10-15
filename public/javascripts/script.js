b9e25e88b08b6478d7055cb28d34eac65b4c139c
Binah-CS <chsh8779@gmail.com>
2025-10-16 02:15:02 +0300

    Fix on neLat/Lang

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -76,7 +76,7 @@ socket.on("receive-location", (data) => {
   if (markers[id]) {
     markers[id].setLatLng([newLat, newLng]);
   } else {
-    markers[id] = L.marker([newLat, newLng]).addTo(markerClusterGroup);
+    markers[id] = L.marker([latitude, longitude]).addTo(markerClusterGroup);
   }
   updateLocationList(id, newLat, newLng);
 });
